abbff3fdd083ff2400b0659603f571294ac77a80
Flopiras <Flopiras@yahoo.it>
2023-06-16 14:21:39 +0200

    add app Vue

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 console.log('js ok');
 console.log('Vue ok', Vue)
+
+const {createApp} = Vue;
+
+const app = createApp({
+    data() {
+        return{
+            
+        }
+    }
+});
+
+app.mount('#root');
